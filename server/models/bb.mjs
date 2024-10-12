19f110048f8930c6e31dd3a6fec1862d947f3fdd
Afam <78809835+n-eon10@users.noreply.github.com>
2024-10-12 13:29:33 +0100

    init repo

---
+++ server/models/bb.mjs
@@ -0,0 +1,20 @@
+// define the schema for black businesses 
+/* 
+We must define their:
+  Name
+  Descriptions 
+  Location
+  Reviews as a list 
+*/ 
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const blackBusinessSchema = new Schema({
+  // define all the black business stuff in here
+
+}, {timestamps: true});
+
+const BlackBusiness = mongoose.model("BlackBusiness", blackBusinessSchema);
+
+export default Event;
